307dea9426ae7bb0cf5562c49c14bd316be9c0e0
ahsan-it <ahsanabid532@gmail.com>
2023-09-01 20:25:42 +0500

    assignment07

---
+++ app.js
@@ -0,0 +1,46 @@
+// !!chapter: 1 !!
+// "Answer 1"
+alert("Assalamualaikum")
+
+// "Answer 2"
+alert("Error!please correct.")
+
+// "Answer 3"
+alert("Welcome to JavaScript..\nHappy coding!")
+
+// "Answer 4"
+alert("Welcome to JavaScript")
+aler("Happy coding!\n▢prevent this page from creating additional dialog")
+
+// "Answer 5"
+let text;
+let text2 = prompt("Please enter your message:");
+alert(text2) 
+
+// !!chapter: 2 !!
+// "Answer 1 and 2" 
+var name="Muhammad"
+var name2="Ahsan"
+console.log( name + name2)
+
+// "Answer 3" 
+alert("Assalamualaikum")
+
+// "Answer 4"
+alert("MuhammadAhsan")
+alert("age : 20")
+alert("certified mobile application development")
+
+// "Answer 5"
+alert("burger \nburge \nburg \nbur \nb")
+    
+// "Answer 6"
+var email="ahsanabid532@gmail.com"
+alert("my email address is "+email)
+
+// "Answer 7"
+var book="I Am Trying To Learn From Smarter Way To Learn JS"
+alert(book)
+
+// "Answer 8"  
+document.writeln("my name is ahsan")
